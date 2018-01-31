6449a40940304706d9fa068efab2563e96335eb1
mfentane <myriam.fentanes@gmail.com>
2018-01-31 16:51:28 -0500

    Changing the name of the app in the script

--- scripts/eap-setup-redhat.sh
+++ scripts/eap-setup-redhat.sh
@@ -39,9 +39,9 @@ echo "Installing GIT" >> /home/$1/install.progress.txt
 yum install -y git >> /home/$1/install.out.txt 2>&1
 
 cd /home/$1
-echo "Getting the sample pollo app to install" >> /home/$1/install.progress.txt
+echo "Getting the sample dukes app to install" >> /home/$1/install.progress.txt
 git clone git@github.com:MyriamFentanes/dukes.git >> /home/$1/install.out.txt 2>&1
-mv /home/$1/pollo/target/pollo $EAP_HOME/standalone/deployments/dukes.war
+mv /home/$1/pollo/target/dukes.war $EAP_HOME/standalone/deployments/dukes.war
 cat > $EAP_HOME/standalone/deployments/dukes.war.dodeploy
 
 echo "Configuring EAP managment user" >> /home/$1/install.progress.txt
